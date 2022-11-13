74bcbf46b7149eb79a98c9771bdbc9bc08b09033
Vladimir Stratila <93329500+Vladimir-Stratila@users.noreply.github.com>
2022-11-13 22:05:47 +0200

    Update memory_info_method_channel.dart

--- lib/memory_info_method_channel.dart
+++ lib/memory_info_method_channel.dart
@@ -8,25 +8,25 @@ import 'memory_info_platform_interface.dart';
 class MethodChannelMemoryInfo extends MemoryInfoPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
-  final methodChannel = const MethodChannel('name.stratila.vladimir/memory_info');
+  final channel = const MethodChannel('name.stratila.vladimir/memory_info');
 
   @override
   Future<String?> platformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version = await channel.invokeMethod<String>('getPlatformVersion');
     return version;
   }
 
   @override
   Future<DiskSpace> diskSpace() async {
     return DiskSpace.fromMap(
-      (await methodChannel.invokeMethod('getDiskSpace')).cast<String, dynamic>(),
+      (await channel.invokeMethod('getDiskSpace')).cast<String, dynamic>(),
     );
   }
 
   @override
   Future<Memory> memoryInfo() async {
     return Memory.fromMap(
-      (await methodChannel.invokeMethod('getMemoryInfo')).cast<String, dynamic>(),
+      (await channel.invokeMethod('getMemoryInfo')).cast<String, dynamic>(),
     );
   }
 }
